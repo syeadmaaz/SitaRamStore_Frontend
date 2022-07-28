f03be958e3fb970afc54ea6c45a61b06aeef99a7
syeadmaaz <syeadmaazahmed@gmail.com>
2022-07-28 11:51:57 +0530

    Updated Code

--- src/Navigation/NavigateScreens.js
+++ src/Navigation/NavigateScreens.js
@@ -9,8 +9,8 @@ import LoginScreen from "../screen/LoginScreen";
 import RegisterScreen from "../screen/RegisterScreen";
 import ProductsScreen from "../screen/ProductsScreen";
 import CartScreen from "../screen/CartScreen";
-import AdminHomescreen from "../components/Admin/screen/AdminHomescreen";
-import MyComponent from "../components/Admin/screen/NewOrder";
+import AdminHomescreen from "../Admin/screen/AdminHomescreen";
+import MyComponent from "../Admin/screen/NewOrder";
 
 const RootStack = createNativeStackNavigator();
 const MainStack = createNativeStackNavigator();
