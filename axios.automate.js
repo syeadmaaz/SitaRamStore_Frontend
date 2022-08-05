187a9197d302f7078b33691148231865974eb383
Amrit Shukla <syeadmaazahmed@fmail.com>
2022-08-05 12:51:06 +0530

    amrit axios

--- axios.automate.js
+++ axios.automate.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.117.139:5000/",
+  // baseURL: "http://192.168.117.139:5000/",
+
+  baseURL: "http://192.168.43.194:5000/",
 });
 
 console.log(api.baseURL);
